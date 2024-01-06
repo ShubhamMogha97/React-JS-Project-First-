fd046cea87876fd8b79279449e262f431561b2cc
Shubham Mogha <shubham.mogha97@gmail.com>
2024-01-06 14:21:41 +0530

    E3

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
-import React from "react" // This refers react inside Node module
-import ReactDOM from "react";
+import React from "react"; // This refers react inside Node module
+import ReactDOM from "react-dom/client";
 
+// React Element
 
-  /* <div id="parent">
-    <div id="child">
-        <h1>Hello World 1</h1>
-         <h1>Hello World 2</h1>
-    </div>
-</div> */
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }),
-  [
-    React.createElement("h1", {}, "Hello world 1"),
-    React.createElement("h1", {}, "Hello world 2"),
-  ]
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "This is a Heading"
 );
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "This is a  Namaste React JS Series"
-// );
-// console.log(heading); This is a object
+const jsxHeading = <h1>Hello world</h1>;
+console.log(jsxHeading);
+
+// Class component
+// Functional component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
